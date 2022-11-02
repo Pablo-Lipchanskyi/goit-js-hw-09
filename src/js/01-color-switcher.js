3964ee58a546fbe01b3707d0de26b7f1e09067d2
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2022-11-02 14:27:04 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,13 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+
+function getRandomHexColor() {
+  return `${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function changeColor() {
+    const interval = setInterval(() => {
+        document.body.style.backgroundColor = `rgb(${getRandomHexColor()})`
+    },1000)
+}
+startBtn.addEventListener('click', changeColor());
