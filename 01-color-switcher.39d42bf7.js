25051507e0041498970fb4e451dd73e6d42ba83c
Pablo-Lipchanskyi <106179379+Pablo-Lipchanskyi@users.noreply.github.com>
2022-11-02 12:27:35 +0000

    Deploying to gh-pages from @ Pablo-Lipchanskyi/goit-js-hw-09@3964ee58a546fbe01b3707d0de26b7f1e09067d2 🚀

---
+++ 01-color-switcher.39d42bf7.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]");document.querySelector("[data-stop]");t.addEventListener("click",void setInterval((()=>{document.body.style.backgroundColor=`rgb(${Math.floor(16777215*Math.random()).toString(16)})`}),1e3));
+//# sourceMappingURL=01-color-switcher.39d42bf7.js.map
