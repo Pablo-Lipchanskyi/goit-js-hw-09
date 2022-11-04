15268b493bbec517402fcb535fd939ca37b2b6d3
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2022-11-04 10:23:25 +0200

    timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,15 +1,38 @@
 import flatpickr from "flatpickr";
-// Дополнительный импорт стилей
 import "flatpickr/dist/flatpickr.min.css";
 
+const input = document.querySelector("#datetime-picker");
+const startBtn = document.querySelector('[data-start]');
+startBtn.addEventListener('click', Dates)
+flatpickr(input, options)
+let selDate = null;
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    selDate = selectedDates[0];
   },
 };
-const input = document.querySelector("#datetime-picker");
-flatpickr(input,options)
+
+function Dates() {
+  if (selDate > Date.now) {
+    console.log('Its a magic')
+  }
+}
+
+
+
+
+
+const timer = {
+  start() {
+    const startTime = Date.now()
+    setInterval(() => {
+      const currentTime = Date.now()
+      console.log(startTime - currentTime)
+    }, 1000)
+  }
+}
+timer.start
